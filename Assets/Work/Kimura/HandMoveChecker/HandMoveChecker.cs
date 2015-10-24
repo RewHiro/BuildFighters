73210be8ef3c4e224605cb6feef26320182a8ce8
kazukin134 <kazukin1345@gmail.com>
2015-10-24 09:37:14 +0900

    lespmotionで前後移動

--- Assets/Work/Kimura/HandMoveChecker/HandMoveChecker.cs
+++ Assets/Work/Kimura/HandMoveChecker/HandMoveChecker.cs
@@ -1,18 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class HandMoveChecker : MonoBehaviour {
+public class HandMoveChecker : MonoBehaviour
+{
 
-    HandController hand_controller_ = null;
-
-    Vector3 hand_pos_;
-    Vector3 hand_old_pos_;
-
-    SkeletalHand skeleta_hand_;
-
-    IsHandExist is_hand_exist_;
-
-    bool once_position_unification_;
 
     public enum MoveType
     {
@@ -21,17 +12,34 @@ public class HandMoveChecker : MonoBehaviour {
         STOP
     }
 
+    HandController hand_controller_ = null;
+
+    SkeletalHand skeleta_hand_;
+
+    IsHandExist is_hand_exist_;
+
     MoveType move_type_;
+
+    Vector3 hand_pos_;
+
+    Vector3 hand_old_pos_;
+
+    bool once_position_unification_;
+
+    [SerializeField, TooltipAttribute("1フレームで動かして判定する距離")]
+    float one_frame_move_distance_ = 0.4f;
+
 
     void Start()
     {
-        is_hand_exist_ = GetComponentInParent<IsHandExist>();
-        once_position_unification_ = false;
-        move_type_ = MoveType.STOP;
         hand_controller_ = FindObjectOfType<HandController>();
         skeleta_hand_ = GetComponent<SkeletalHand>();
-        hand_old_pos_ = skeleta_hand_.GetPalmPosition();
+        is_hand_exist_ = GetComponentInParent<IsHandExist>();
+
+        once_position_unification_ = false;
+        move_type_ = MoveType.STOP;
         hand_pos_ = skeleta_hand_.GetPalmPosition();
+        hand_old_pos_ = hand_pos_;
     }
 
 
@@ -41,27 +49,13 @@ public class HandMoveChecker : MonoBehaviour {
         Debug.Log(move_type_);
     }
 
-    void HandMoveCheck()
+    public MoveType HandMoveCheck()
     {
-
-        //foreach (var hand in FindObjectsOfType<SkeletalHand>())
-        //{
-
-        //    hand_pos_ = hand.GetPalmPosition();
-
-        //    if (hand_pos_.z > hand_old_pos_.z + 0.3f)
-        //    {
-
-        //        transform.Translate(0, 0, 0.1f);
-        //        return;
-        //    }
-        //    hand_old_pos_ = hand_pos_;
-        //}
 
         hand_pos_ = skeleta_hand_.GetPalmPosition();
 
 
-        if (is_hand_exist_.IsExistence() == true 
+        if (is_hand_exist_.IsExistence() == true
             && once_position_unification_ == false)
         {
             hand_old_pos_ = hand_pos_;
@@ -75,23 +69,25 @@ public class HandMoveChecker : MonoBehaviour {
             once_position_unification_ = false;
         }
 
-        if (hand_pos_.z > hand_old_pos_.z + 0.4f)
+        if (hand_pos_.z > hand_old_pos_.z + one_frame_move_distance_)
         {
             move_type_ = MoveType.ADVANCE;
 
         }
         else
-        if(hand_pos_.z < hand_old_pos_.z - 0.4f)
+        if (hand_pos_.z < hand_old_pos_.z - one_frame_move_distance_)
         {
             move_type_ = MoveType.BACKWARD;
 
-        }else
+        }
+        else
         {
             move_type_ = MoveType.STOP;
             hand_old_pos_ = hand_pos_;
 
         }
 
+        return move_type_;
 
     }
 
