99fb4895be40d2b96bfdf5580a137df043b8a516
kazukin134 <kazukin1345@gmail.com>
2015-10-24 12:40:07 +0900

    命名規則修正とマジックナンバーを消した

--- Assets/Work/Kimura/HandMoveChecker/PlayerMover.cs
+++ Assets/Work/Kimura/HandMoveChecker/PlayerMover.cs
@@ -4,46 +4,36 @@ using System.Collections;
 public class PlayerMover : MonoBehaviour
 {
 
-    HandMoveChecker[] hand_move_checker_;
+    HandMoveChecker[] hand_move_checkers_;
 
-    public HandMoveChecker[] Hand_move_checker_
-    {
-        get
-        {
-            return hand_move_checker_;
-        }
-
-        set
-        {
-            hand_move_checker_ = value;
-        }
-    }
+   const int ONE_HAND_DISALLOW_ = 1;
 
     void Start()
     {
-        hand_move_checker_ = FindObjectsOfType<HandMoveChecker>();
+        hand_move_checkers_ = FindObjectsOfType<HandMoveChecker>();
     }
 
 
     void Update()
     {
         BothhandsPush();
+        Debug.Log(hand_move_checkers_.Length);
     }
 
     void BothhandsPush()
     {
-        hand_move_checker_ = FindObjectsOfType<HandMoveChecker>();
+        hand_move_checkers_ = FindObjectsOfType<HandMoveChecker>();
 
-        if (!(hand_move_checker_.Length <= 1)) return;
+        if (hand_move_checkers_.Length <= ONE_HAND_DISALLOW_) return;
 
-        if (hand_move_checker_[0].HandMoveCheck() == HandMoveChecker.MoveType.ADVANCE 
-            && hand_move_checker_[1].HandMoveCheck() == HandMoveChecker.MoveType.ADVANCE)
+        if (hand_move_checkers_[0].HandMoveCheck() == HandMoveChecker.MoveType.ADVANCE 
+            && hand_move_checkers_[1].HandMoveCheck() == HandMoveChecker.MoveType.ADVANCE)
         {
             transform.Translate(0, 0, 0.1f);
         }
         else
-        if (hand_move_checker_[0].HandMoveCheck() == HandMoveChecker.MoveType.BACKWARD
-            && hand_move_checker_[1].HandMoveCheck() == HandMoveChecker.MoveType.BACKWARD)
+        if (hand_move_checkers_[0].HandMoveCheck() == HandMoveChecker.MoveType.BACKWARD
+            && hand_move_checkers_[1].HandMoveCheck() == HandMoveChecker.MoveType.BACKWARD)
         {
             transform.Translate(0, 0, -0.1f);
         }
