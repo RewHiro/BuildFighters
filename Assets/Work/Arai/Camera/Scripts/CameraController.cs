efad0f4572c3e2546927a4dc52da3bc4e2c860fb
ooHIROoo <hiro1215.turquoise@gmail.com>
2015-10-25 22:42:18 +0900

    デバッグ用カメラ操作実装

---
+++ Assets/Work/Arai/Camera/Scripts/CameraController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+    readonly Vector2 LIMIT_SLOPE = new Vector2(70.0f, 80.0f);
+
+    [SerializeField]
+    float SWING_SPEED = 5.0f;
+
+    void Update()
+    {
+        var mouse_x = Input.GetAxis("Mouse X");
+        var mouse_y = Input.GetAxis("Mouse Y");
+
+        if (mouse_x == 0.0f && mouse_y == 0.0f) return;
+
+        gameObject.transform.Rotate(Vector3.up, mouse_x * SWING_SPEED);
+
+        var euler_angles = gameObject.transform.localEulerAngles;
+
+        euler_angles = ConvertPlsuMinus180(euler_angles);
+
+        euler_angles.y = Mathf.Clamp(euler_angles.y, -LIMIT_SLOPE.y, LIMIT_SLOPE.y);
+
+        euler_angles = Convert0To360(euler_angles);
+
+        gameObject.transform.localEulerAngles = euler_angles;
+
+    }
+
+
+    float ConvertPlsuMinus180(float euler_angle)
+    {
+        return euler_angle > 180 ?
+            euler_angle - 360 : euler_angle;
+    }
+
+    Vector3 ConvertPlsuMinus180(Vector3 euler_angles)
+    {
+        var temp_euler_angles = Vector3.zero;
+
+        temp_euler_angles.x = ConvertPlsuMinus180(euler_angles.x);
+        temp_euler_angles.y = ConvertPlsuMinus180(euler_angles.y);
+        temp_euler_angles.z = ConvertPlsuMinus180(euler_angles.z);
+
+        return temp_euler_angles;
+    }
+
+    float Convert0To360(float euler_angle)
+    {
+        return euler_angle < 0 ?
+            euler_angle + 360 : euler_angle;
+    }
+
+    Vector3 Convert0To360(Vector3 euler_angles)
+    {
+        var temp_euler_angles = Vector3.zero;
+
+        temp_euler_angles.x = Convert0To360(euler_angles.x);
+        temp_euler_angles.y = Convert0To360(euler_angles.y);
+        temp_euler_angles.z = Convert0To360(euler_angles.z);
+
+        return temp_euler_angles;
+    }
+}
