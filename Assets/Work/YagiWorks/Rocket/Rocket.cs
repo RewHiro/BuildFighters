6c87c3c6b96f7f32409d72629d37160552769fde
Mellon <Chocomel@apple-no-MacBook-Air.local>
2015-10-29 17:16:46 +0900

    1029学校分

---
+++ Assets/Work/YagiWorks/Rocket/Rocket.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+public class Rocket : MonoBehaviour
+{
+
+    float bulletSpeed = 0.0f;
+
+    public float life_time = 1.5f;
+    float time = 0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        time = 0;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time > life_time)
+        {
+            Destroy(gameObject);
+        }
+        transform.Translate(0, 0, bulletSpeed * bulletSpeed);
+        bulletSpeed += 0.016f;
+    }
+}
---
+++ Assets/Work/YagiWorks/Rocket/RocketPlayer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RocketPlayer : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject bullet;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("up"))
+        {
+            transform.position += transform.forward;
+        }
+
+        if (Input.GetKey("right"))
+        {
+            transform.Rotate(0, 5, 0);
+        }
+        if (Input.GetKey("left"))
+        {
+            transform.Rotate(0, -5, 0);
+        }
+        if (Input.GetKeyDown("space"))
+        {
+            Instantiate(bullet, new Vector3(transform.position.x, transform.position.y, transform.position.z), transform.rotation);
+        }
+    }
+
+}
