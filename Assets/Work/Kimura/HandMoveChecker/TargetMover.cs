a0258bf0cb50c925600a134a8ae91ea86bbec089
Mellon <chocco.mellon.2525@gmail.com>
2015-11-01 21:10:03 +0900

    no message

---
+++ Assets/Work/Kimura/HandMoveChecker/TargetMover.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetMover : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            transform.position += transform.forward;
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.position -= transform.forward;
+        }
+
+        if (Input.GetKey("d"))
+        {
+            transform.Rotate(0, 5, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.Rotate(0, -5, 0);
+        }
+
+    }
+}
