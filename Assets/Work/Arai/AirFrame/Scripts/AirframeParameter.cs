a3266b2b8d077b3abbd91d33dfd46163e9e2a88e
ooHIROoo <hiro1215.turquoise@gmail.com>
2015-11-01 16:29:52 +0900

    LitJson用に読み込み変更

--- Assets/Work/Arai/AirFrame/Scripts/AirframeParameter.cs
+++ Assets/Work/Arai/AirFrame/Scripts/AirframeParameter.cs
@@ -1,6 +1,10 @@
 ﻿using UnityEngine;
 using System.IO;
 using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using LitJson;
+using System;
 
 public class AirFrameParameter : MonoBehaviour
 {
@@ -13,93 +17,104 @@ public class AirFrameParameter : MonoBehaviour
     public float GetMass(int id)
     {
         return parameters_.ContainsKey(id) ?
-            parameters_[id].mass_ : ErrorMessageFloat(id);
+            parameters_[id].Mass : ErrorMessageFloat(id);
     }
 
     public float GetDrag(int id)
     {
         return parameters_.ContainsKey(id) ?
-            parameters_[id].drag_ : ErrorMessageFloat(id);
+            parameters_[id].Drag : ErrorMessageFloat(id);
     }
 
     public float GetMoveSpeed(int id)
     {
         return parameters_.ContainsKey(id) ?
-            parameters_[id].move_speed_ : ErrorMessageFloat(id);
+            parameters_[id].MoveSpeed : ErrorMessageFloat(id);
     }
 
     public float GetSwingSpeed(int id)
     {
         return parameters_.ContainsKey(id) ?
-            parameters_[id].swing_speed_ : ErrorMessageFloat(id);
+            parameters_[id].SwingSpeed : ErrorMessageFloat(id);
     }
 
     public float GetBoostPower(int id)
     {
         return parameters_.ContainsKey(id) ?
-            parameters_[id].boost_power_ : ErrorMessageFloat(id);
+            parameters_[id].BoostPower : ErrorMessageFloat(id);
     }
 
     public float GetJumpPower(int id)
     {
         return parameters_.ContainsKey(id) ?
-            parameters_[id].jump_power_ : ErrorMessageFloat(id);
+            parameters_[id].JumpPower : ErrorMessageFloat(id);
     }
 
     public float GetMaxBoostValue(int id)
     {
         return parameters_.ContainsKey(id) ?
-            parameters_[id].max_boost_value_ : ErrorMessageFloat(id);
+            parameters_[id].MaxBoostValue : ErrorMessageFloat(id);
     }
 
     public float GetMaxHP(int id)
     {
         return parameters_.ContainsKey(id) ?
-            parameters_[id].max_hp_ : ErrorMessageFloat(id);
+            parameters_[id].MaxHP : ErrorMessageFloat(id);
     }
 
     public float GetArmorValue(int id)
     {
         return parameters_.ContainsKey(id) ?
-            parameters_[id].armor_value_ : ErrorMessageFloat(id);
+            parameters_[id].ArmorValue : ErrorMessageFloat(id);
     }
 
     public string GetName(int id)
     {
         return parameters_.ContainsKey(id) ?
-            parameters_[id].name_ : ErrorMessageString(id);
+            parameters_[id].Name : ErrorMessageString(id);
     }
 
     public string GetExplanatoryText(int id)
     {
         return parameters_.ContainsKey(id) ?
-            parameters_[id].explanatory_text_ : ErrorMessageString(id);
+            parameters_[id].ExplanatoryText : ErrorMessageString(id);
     }
 
     void Awake()
     {
+        JsonMapper.RegisterExporter<float>((obj, writer) => writer.Write(Convert.ToDouble(obj)));
+
         var json_text = File.ReadAllText(Application.dataPath + JSON_PATH + JSON_FILE_NAME);
-        JsonNode json = JsonNode.Parse(json_text);
-        var parameters = json["Parameters"];
-        foreach (var parameter in parameters)
+        var json_data = JsonMapper.ToObject(json_text);
+        var parameters = json_data["Parameters"];
+
+
+        for (var i = 0; i < parameters.Count; i++)
         {
             Parameter temp_parameter;
-            temp_parameter.id_ = (int)parameter["ID"].Get<long>();
-            temp_parameter.mass_ = (float)parameter["Mass"].Get<double>();
-            temp_parameter.drag_ = (float)parameter["Drag"].Get<double>();
-            temp_parameter.move_speed_ = (float)parameter["MoveSpeed"].Get<double>();
-            temp_parameter.swing_speed_ = (float)parameter["SwingSpeed"].Get<double>();
-            temp_parameter.boost_power_ = (float)parameter["BoostPower"].Get<double>();
-            temp_parameter.jump_power_ = (float)parameter["JumpPower"].Get<double>();
-            temp_parameter.max_boost_value_ = (float)parameter["MaxBoostValue"].Get<double>();
-            temp_parameter.max_hp_ = (float)parameter["MaxHP"].Get<double>();
-            temp_parameter.armor_value_ = (float)parameter["ArmorValue"].Get<double>();
-            temp_parameter.name_ = parameter["Name"].Get<string>();
-            temp_parameter.explanatory_text_ = parameter["ExplanatoryText"].Get<string>();
-            
 
-            parameters_.Add(temp_parameter.id_, temp_parameter);
+            temp_parameter.ID = (int)parameters[i]["ID"];
+            temp_parameter.Mass = (float)(double)parameters[i]["Mass"];
+            temp_parameter.Drag = (float)(double)parameters[i]["Drag"];
+            temp_parameter.MoveSpeed = (float)(double)parameters[i]["MoveSpeed"];
+            temp_parameter.SwingSpeed = (float)(double)parameters[i]["SwingSpeed"];
+            temp_parameter.BoostPower = (float)(double)parameters[i]["BoostPower"];
+            temp_parameter.JumpPower = (float)(double)parameters[i]["JumpPower"];
+            temp_parameter.MaxBoostValue = (float)(double)parameters[i]["MaxBoostValue"];
+            temp_parameter.MaxHP = (float)(double)parameters[i]["MaxHP"];
+            temp_parameter.ArmorValue = (float)(double)parameters[i]["ArmorValue"];
+            temp_parameter.Name = (string)parameters[i]["Name"];
+            temp_parameter.ExplanatoryText = (string)parameters[i]["ExplanatoryText"];
+
+            parameters_.Add(temp_parameter.ID, temp_parameter);
+            parameters_test_.Add(temp_parameter);
         }
+
+        json_.Add("Parameters", parameters_test_);
+
+        string test = JsonMapper.ToJson(json_);
+
+        Debug.Log(test);
     }
 
     // いい方法が思いつかない……
@@ -117,20 +132,23 @@ public class AirFrameParameter : MonoBehaviour
 
     struct Parameter
     {
-        public int id_;
-        public float mass_;
-        public float drag_;
-        public float move_speed_;
-        public float swing_speed_;
-        public float boost_power_;
-        public float jump_power_;
-        public float max_boost_value_;
-        public float max_hp_;
-        public float armor_value_;
-        public string name_;
-        public string explanatory_text_;
+        public int ID;
+        public float Mass;
+        public float Drag;
+        public float MoveSpeed;
+        public float SwingSpeed;
+        public float BoostPower;
+        public float JumpPower;
+        public float MaxBoostValue;
+        public float MaxHP;
+        public float ArmorValue;
+        public string Name;
+        public string ExplanatoryText;
     }
 
+
+    Dictionary<string, List<Parameter>> json_ = new Dictionary<string, List<Parameter>>();
+    List<Parameter> parameters_test_ = new List<Parameter>();
     Dictionary<int, Parameter> parameters_ = new Dictionary<int, Parameter>();
 
 }
