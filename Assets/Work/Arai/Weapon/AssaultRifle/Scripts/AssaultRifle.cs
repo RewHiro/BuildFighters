01ddfcdb129b25727203ec599845df255878310a
ooHIROoo <hiro1215.turquoise@gmail.com>
2015-11-01 16:37:37 +0900

    アサルトライフルの実装、大元になるWeaponクラス実装、弾生成管理追加

---
+++ Assets/Work/Arai/Weapon/AssaultRifle/Scripts/AssaultRifle.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AssaultRifle : Weapon
+{
+
+    //Speedを大きくしすぎるとみえなくなるから注意
+    [SerializeField]
+    float Speed = 100;//速度
+
+    GameObject bullet = null;
+
+    void Start()
+    {
+        bullet = FindObjectOfType<BulletCreater>().getBullet;
+    }
+
+    public override void Attack()
+    {
+        var obj = Instantiate(bullet);
+        obj.transform.position = gameObject.transform.position;
+        obj.transform.Translate(gameObject.transform.forward * 2.5f);
+        Debug.Log(obj.transform.position);
+        Vector3 force;
+        force = gameObject.transform.forward * Speed;
+        obj.GetComponent<Rigidbody>().AddForce(force, ForceMode.Impulse);
+    }
+}
---
+++ Assets/Work/Arai/Weapon/Scripts/Weapon.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon : MonoBehaviour
+{
+    virtual public void Attack() { }
+
+    protected int id_ = 0;
+    protected string name_ = "";
+    protected string explanatory_text_ = "";
+}
