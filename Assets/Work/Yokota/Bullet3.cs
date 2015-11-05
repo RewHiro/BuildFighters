10820b2a3368cb8a5de87ddbc031ebfabf03b619
lycoris009ky <lycoris2525@gmail.com>
2015-11-05 18:19:55 +0900

    1105 18

---
+++ Assets/Work/Yokota/Bullet3.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet3 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+                if (col.gameObject.tag == "enemy")
+        {
+            Debug.Log("hit");
+            Destroy(gameObject);
+        }
+    }
+}
